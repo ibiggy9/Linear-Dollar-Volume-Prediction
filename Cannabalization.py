34d522edb2c955bca6d66a51c30e27b909d26dff
Ian B <44586707+ibiggy9@users.noreply.github.com>
2023-09-25 16:45:49 -0400

    Update Cannabalization.py

--- Cannabalization.py
+++ Cannabalization.py
@@ -1,160 +1,102 @@
+# Import necessary libraries
 import pandas as pd
 import numpy as np
-from pandas.core.frame import DataFrame
-import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 from sklearn import metrics
 import pickle
-import seaborn as seabornInstance
-from sklearn import metrics
-import matplotlib.pyplot as pltd
-import math
 import datetime
 
-#Algorithm efficiency clock
+# Start the efficiency clock
 begin_time = datetime.datetime.now()
 
-#Read Data
+# Read data from CSV and assign to DataFrame
 data = pd.read_csv("Source of volume.csv")
 
-#Prediction metric
+# Define the target variable
 predict = "Cheez It"
 
-#Data Input
-data = data[[
-    "RITZ", 
-    "Triscuits",
-    "Bear Paws Crackers", 
-    "Wheat Thins", 
-    "Goldfish", 
-    "Keebler Townhouse", 
-    "Cheez It"
-]]
+# Extract relevant features from data
+data = data[
+    [
+        "RITZ",
+        "Triscuits",
+        "Bear Paws Crackers",
+        "Wheat Thins",
+        "Goldfish",
+        "Keebler Townhouse",
+        "Cheez It",
+    ]
+]
 
-#Shuffle data for training the model
-data = shuffle(data) 
+# Shuffle the data to randomize the entries
+data = shuffle(data)
 
-#how to display data
-#print(data.head())
-#print(data.tail(n=10))
-
-#Putting the inputs in their correct place in the algorithm 
+# Split data into training and test sets
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-#Running the calculation 
+# Initialize and train the linear regression model
 linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
 
-#Testing the accuracy of the model + Output
+# Calculate the accuracy of the model
+acc = linear.score(x_test, y_test)
 y_pred = linear.predict(x_test)
 
-print("Accuracy: " + str(acc))
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-print('Root Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+# Print initial model metrics
+print("Initial Model Metrics:")
+print("----------------------")
+print("Accuracy:", acc)
+print("Mean Absolute Error:", metrics.mean_absolute_error(y_test, y_pred))
+print("Mean Squared Error:", metrics.mean_squared_error(y_test, y_pred))
+print("Root Mean Squared Error:", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
-print('Coefficient: \n', linear.coef_)
-print('Intercept: \n', linear.intercept_)
-
-# TRAIN MODEL MULTIPLE TIMES FOR BEST SCORE
-
-#Target minimum best accuracy
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+# Train the model multiple times to find the best score
 best = 0.6
 count = 0
-
 for _ in range(10000):
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
-
+    
     linear = linear_model.LinearRegression()
     linear.fit(x_train, y_train)
+    
     acc = linear.score(x_test, y_test)
-    # The line below is needed for error testing.
     y_pred = linear.predict(x_test)
+    
     count += 1
+    
     print(f"\nRun number: {count}")
-    print("Accuracy: " + str(acc))
-    print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-    print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-    print('Root Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+    print("Accuracy:", acc)
+    print("Mean Absolute Error:", metrics.mean_absolute_error(y_test, y_pred))
+    print("Mean Squared Error:", metrics.mean_squared_error(y_test, y_pred))
+    print("Root Mean Squared Error:", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+    
+    if acc > best:
+        best = acc
+        with open("cannabal.pickle", "wb") as f:
+            pickle.dump(linear, f)
 
-#Displaying the accuracy
-if acc > best:
-    best = acc
-    #Saving the model
-    with open("cannabal.pickle", "wb") as f:
-        pickle.dump(linear, f)
+# Load the best model
+with open("cannabal.pickle", "rb") as pickle_in:
+    linear = pickle.load(pickle_in)
 
-print(best)
+# Calculate and print final results
+final_accuracy = int(acc * 100)
+coefficients = list(zip(data.columns[:-1], linear.coef_))
 
-# LOAD MODEL 
-pickle_in = open("cannabal.pickle", "rb") 
-linear = pickle.load(pickle_in)
-final_accuracy = int(acc*100)
-coeff = {}
-arr = linear.coef_
+print("\nResults:")
+print("-------")
+print(f"Total run time: {datetime.datetime.now() - begin_time} seconds")
+print(f"Total number of runs: {count}")
+print("Intercept:", linear.intercept_)
+print(f"Best accuracy: {final_accuracy}%")
+print("Mean Absolute Error:", metrics.mean_absolute_error(y_test, y_pred))
+print("Mean Squared Error:", metrics.mean_squared_error(y_test, y_pred))
+print("Root Mean Squared Error:", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
-a_zip = zip(data,arr)
-ziper = list(a_zip)
-run_time = datetime.datetime.now() - begin_time
-print('\n')
-print("------------------------------------------------------------------")
-print("The Results Are:")
-print("------------------------------------------------------------------")
-print('\n')
-print('\n')
-print('\n')
-
-empty = []
-#Iteration through results to print final cannibalization score
-for index, value in ziper:
-    empty.append(value)
-
-avg_can = int(((sum(empty)/15))*100)
-binomial_effect_size_display_pos = (final_accuracy/2)+50
-binomial_effect_size_display_neg = (final_accuracy/2)-50
-print("------------------------------------------------------------------")
-print("Accuracy Statistics")
-print("------------------------------------------------------------------")
-print(f"Chance of being accurate is {binomial_effect_size_display_pos}%")
-print(f'Chance of being inaccurate is {binomial_effect_size_display_neg}%')
-
-
-print('\n')
-print("------------------------------------------------------------------")
-print("Output Report:")
-print("------------------------------------------------------------------")
-for i, x in ziper:
-    a = x*100
-    print(f"Cheez It sourced{a}% volume from {i}")
-
-
-print(f"\nTotal run time: \n{run_time} seconds\n")
-print(f"Total number of runs: \n{count}\n")
-print('Intercept: \n', linear.intercept_)
-print(f'\nBest accuracy: \n{final_accuracy}%')
-print('\nMean Absolute Error:\n', metrics.mean_absolute_error(y_test, y_pred))
-print('\nMean Squared Error:\n', metrics.mean_squared_error(y_test, y_pred))
-print('\nRoot Squared Error:\n', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-print("------------------------------------------------------------------")
-
-
-#plot the data
-#data.describe()
-
-#Check for null values
-#data.isnull().any()
-
-#Remove null data
-# data = data.fillna(method='ffill')
-
-"""
-#HOW TO PLUG IN VALUES TO PREDICT $VOL 
-new_x = [[2, 90, 2.3, 2.4, 2, 4.12]]
-print('The Expected $Vol with your inputs is:\n ', linear.predict(new_x))
-
-"""
+print("\nCannibalization Scores:")
+print("----------------------")
+for feature, coef in coefficients:
+    print(f"Cheez It sourced {coef*100:.2f}% volume from {feature}")
